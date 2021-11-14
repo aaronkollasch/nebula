b92a1e96a32a74c166de3fa7479c10328be1a5ba
Aaron Kollasch <awkollasch@gmail.com>
2021-11-14 11:43:47 -0500

    Make network private in Windows
    Uses Set-NetConnectionProfile to make the network private when the connection is created.
    Prints stderr from the command to assist debugging, and does not fail if Set-NetConnectionProfile fails.
    No config option included, so not intended as a general solution.

--- overlay/tun_water_windows.go
+++ overlay/tun_water_windows.go
@@ -6,6 +6,7 @@ import (
 	"net"
 	"os/exec"
 	"strconv"
+	"bytes"
 
 	"github.com/slackhq/nebula/cidr"
 	"github.com/slackhq/nebula/iputil"
@@ -90,6 +91,22 @@ func (t *waterTun) Activate() error {
 		if err != nil {
 			return fmt.Errorf("failed to add the unsafe_route %s: %v", r.Cidr.String(), err)
 		}
+	}
+
+
+	cmd := exec.Command(
+		"C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe", "-Command",
+		fmt.Sprintf("Set-NetConnectionProfile -InterfaceAlias \"%s\" -NetworkCategory Private", t.Device),
+	)
+	var out bytes.Buffer
+	var stderr bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Stderr = &stderr
+	err = cmd.Run()
+	if err != nil {
+		fmt.Println(out.String())
+		fmt.Println(stderr.String())
+		fmt.Errorf("failed to set connection profile for interface %s: %v\n", t.Device, err)
 	}
 
 	return nil
--- overlay/tun_wintun_windows.go
+++ overlay/tun_wintun_windows.go
@@ -6,6 +6,8 @@ import (
 	"io"
 	"net"
 	"unsafe"
+	"os/exec"
+	"bytes"
 
 	"github.com/slackhq/nebula/cidr"
 	"github.com/slackhq/nebula/iputil"
@@ -119,6 +121,21 @@ func (t *winTun) Activate() error {
 
 	if err := ipif.Set(); err != nil {
 		return fmt.Errorf("failed to set ip interface: %w", err)
+	}
+
+	cmd := exec.Command(
+		"C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe", "-Command",
+		fmt.Sprintf("Set-NetConnectionProfile -InterfaceAlias \"%s\" -NetworkCategory Private", t.Device),
+	)
+	var out bytes.Buffer
+	var stderr bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Stderr = &stderr
+	err = cmd.Run()
+	if err != nil {
+		fmt.Println(out.String())
+		fmt.Println(stderr.String())
+		fmt.Errorf("failed to set connection profile for interface %s: %v\n", t.Device, err)
 	}
 
 	return nil
